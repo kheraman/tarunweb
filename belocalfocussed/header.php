c0f265d0bc201844d313d12ca3fefbfdc7d9fb20
Anurag <50268302+i-m-anurag@users.noreply.github.com>
2024-12-20 22:28:42 +0530

    Update header.php

--- belocalfocussed/header.php
+++ belocalfocussed/header.php
@@ -1,6 +1,6 @@
 <?php
 //session_start();
-//error_reporting(0);
+error_reporting(1);
 require_once('includes/config.php');
 
 $bname = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
