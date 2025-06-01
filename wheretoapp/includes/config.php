87397a5743c616d68da583b96e32439be4b72d54
kheraman <khe.raman25@gmail.com>
2025-06-01 22:19:17 +0530

    1june

--- wheretoapp/includes/config.php
+++ wheretoapp/includes/config.php
@@ -32,9 +32,9 @@ if (!$con->real_connect($host, $user, $pass, $dbname, $port)) {
 global $my_global_variable,$cate_list,$response;
 
 // Set the value of the global variable
-//$my_global_variable = "https://wheretoapp.io/";
+$my_global_variable = "https://wheretoapp.io/";
 
-$my_global_variable = "http://localhost/Synup_LIVE_websites/SYNUP/wheretoapp/";
+//$my_global_variable = "http://localhost/Synup_LIVE_websites/SYNUP/wheretoapp/";
 
 
 
